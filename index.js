d7e812bef5cd822c0b70d3f40349a1663ad00de6
Carlie Duperval <carlierichell@gmail.com>
2022-03-23 12:49:24 -0400

    Update filter on Animal API

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 import express from "express";
-import { getAllCountries, createCountry } from "./src/country.js";
+import { getAllCountries, createCountry, updateCountry, getCountryById, getCountryByFilter } from "./src/country.js";
 const app = express();
 app.use(express.json());
 
-app.get("/countries", async (req, res) => {
+// app.get("/countries", async (req, res) => {
+//   try {
+//     const result = await getCountryById(id);
+//     res.status(200).send(result);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
+
+
+app.get('/countries/:id', async (req, res) => {
+  const { id } = req.params;
   try {
-    const result = await getAllCountries();
-    res.status(200).send(result);
+    const result = await getCountryById (id);
+    res.status(200).send(result)
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).send(error)
   }
-});
+
+})
+
+app.get('/countries', async (req, res) => {
+  const { areacode, location, name, population} = req.query
+  const filter = { areacode, location, name, population}
+  try {
+    const result = await getCountryByFilter(filter)
+    res.status(200).send(result)
+    
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error)
+    
+  }
+})
+
+
 
 app.post("/countries", async (req, res) => {
   const country = req.body;
@@ -22,6 +50,25 @@ app.post("/countries", async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+app.patch('/countries/:id', async (req, res) => {
+  const { id } = req.params
+  const updateInput = req.body
+
+  if (!updateInput){
+    res.status(400).send("Empty Body")
+    return
+  }
+  try {
+    const result = await updateCountry(id, updateInput)
+    res.status(200).send(result)
+    
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+
 
 const port = 5601;
 app.listen(port, () => {
--- src/country.js
+++ src/country.js
@@ -19,10 +19,72 @@ export const getAllCountries = async () => {
 
 export const createCountry = async (contry) => {
   try {
-    const result = await countryCollection.add(contry);
+    const result = await countryCollection.add(contry)
     contry.id = result.id;
     return contry;
   } catch (error) {
     console.error(error);
   }
 };
+
+export const updateCountry = async (id, country) => {
+
+  try {
+     await countryCollection.doc(id).update(country)
+    return await getCountryById(id)
+    
+  } catch (error) {
+    console.error(error)
+  }
+
+}
+
+export const getCountryById = async id => {
+  try {
+    const result = await countryCollection.doc(id).get()
+    return {
+      id: result.id,
+      ...result.data()
+    }
+  } catch (error) {
+    console.error(error)
+    
+  }
+}
+
+export const getCountryByFilter = async countryFilter => {
+
+  if(!countryFilter){
+    countryFilter = {}
+  }
+  const { areacode, location, name, population} = countryFilter
+  let query = countryCollection
+
+
+  if(areacode){
+    query = query.where("areacode", "==", areacode)
+  }
+  if(location){
+    query = query.where("location", "==", location)
+  }
+  if(name){
+    query = query.where("name", "==", name)
+  }
+  if(population){
+    query = query.where("population", "==", population)
+  }
+  
+  try {
+    const snapshot = await query.get()
+    const result = snapshot.docs.map((doc) => {
+      const country = doc.data();
+      country.id = doc.id;
+      return country;
+    });
+    return result
+
+  } catch (error) {
+    console.error(error)
+    
+  }
+}
